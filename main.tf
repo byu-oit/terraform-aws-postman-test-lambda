48bfc69b00f02658aad2a427c6cef8c5fd32bcbc
yoshutch <yoshutch@gmail.com>
2020-07-10 08:26:24 -0600

    wip copied lambda code and getting tf started

--- main.tf
+++ main.tf
@@ -5,3 +5,65 @@ terraform {
   }
 }
 
+resource "aws_lambda_function" "test_lambda" {
+  filename         = "../../../tst/codedeploy-hooks/after-allow-test-traffic/lambda.zip"
+  function_name    = "${local.name}-deploy-test-${var.env}"
+  role             = aws_iam_role.test_lambda.arn
+  handler          = "index.handler"
+  runtime          = "nodejs12.x"
+  timeout          = 30
+  source_code_hash = filebase64sha256("../../../tst/codedeploy-hooks/after-allow-test-traffic/lambda.zip")
+  environment {
+    variables = {
+      "ENV" = var.env
+    }
+  }
+}
+
+resource "aws_iam_role" "test_lambda" {
+  name                 = "${local.name}-deploy-test-${var.env}"
+  permissions_boundary = module.acs.role_permissions_boundary.arn
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "lambda.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy" "test_lambda" {
+  name = "${local.name}-deploy-test-${var.env}"
+  role = aws_iam_role.test_lambda.name
+
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": [
+        "logs:CreateLogGroup",
+        "logs:CreateLogStream",
+        "logs:PutLogEvents"
+      ],
+      "Resource": "arn:aws:logs:*:*:*",
+      "Effect": "Allow"
+    },
+    {
+      "Action": "codedeploy:PutLifecycleEventHookExecutionStatus",
+      "Resource": "*",
+      "Effect": "Allow"
+    }
+  ]
+}
+EOF
+}
