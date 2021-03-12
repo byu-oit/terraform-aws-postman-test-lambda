372e163e453093c01b35a97b305be22ed5c94fb4
yoshutch <yoshutch@gmail.com>
2021-03-12 11:39:10 -0700

    updated advanced example to have multiple collections/environments

---
+++ examples/advanced-example/advanced-example.tf
@@ -0,0 +1,29 @@
+provider "aws" {
+  version = "~> 3.0"
+  region  = "us-west-2"
+}
+
+data aws_ssm_parameter role_permissions_boundary_arn {
+  name = "/acs/iam/iamRolePermissionBoundary"
+}
+
+variable "postman_api_key" {
+  type = string
+}
+
+module "postman_test_lambda" {
+  source   = "../../"
+  app_name = "advanced-example"
+  postman_collections = [
+    {
+      collection  = "1117094-d4bd5a5f-c37c-4fe9-8723-3c3e8b1e2015" # terraform-aws-postman-test-lambda-example collection from postman TF Modules and HW Examples workspace
+      environment = "1117094-95627910-aeb0-4aed-b959-7e2034e2f6ce" # terraform-aws-postman-test-lambda-env environment from postman TF Modules and HW Examples workspace
+    },
+    {
+      collection  = "terraform-aws-postman-test-lambda-example.postman_collection.json"
+      environment = "terraform-aws-postman-test-lambda-env.postman_environment.json"
+    }
+  ]
+  postman_api_key               = var.postman_api_key
+  role_permissions_boundary_arn = data.aws_ssm_parameter.role_permissions_boundary_arn.value
+}
